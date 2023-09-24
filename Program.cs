c0b0ffc4211718dae0a5070c584f548b4a4d7754
Ihor <flisfish@ukr.net>
2023-09-24 23:32:09 +0200

    end of section 2

---
+++ Program.cs
@@ -0,0 +1,44 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<StoreContex>(opt => 
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+var scope = app.Services.CreateScope();
+var contex = scope.ServiceProvider.GetRequiredService<StoreContex>();
+var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+try
+{
+    contex.Database.Migrate();
+    DbInitializer.Initializer(contex);
+}
+catch(Exception ex)
+{
+    logger.LogError(ex, "A problem occurred during migration");
+}
+
+app.Run();
+ 
